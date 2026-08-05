e11ac7b2e8dcc9c0f81c1a808248e0b0ec5d1de6
Sachin Rajpurohit <webpress0325@gmail.com>
2026-08-05 18:51:04 +0530

    v5.3.1: Added Doc-tests suite in src/lib.rs (23/23 integration unit tests + 3/3 doc-tests = 26/26 passing tests total)

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,25 @@
+//! `rcurl`: High-Performance 16-Thread Tokio Protocol Suite (cURL, Wget, Rsync, Rsync-SSL, Rsyncd).
+//!
+//! # Example Doc-Tests
+//! ```rust
+//! use rcurl::modules::rsync::RsyncEngine;
+//! let checksum = RsyncEngine::compute_rolling_checksum(b"Doc-test example");
+//! assert!(checksum > 0);
+//! ```
+//!
+//! ```rust
+//! use rcurl::modules::rsync::RsyncSslEngine;
+//! let ssl_engine = RsyncSslEngine::new(Some("openssl".to_string()), Some(874));
+//! assert_eq!(ssl_engine.ssl_port, 874);
+//! ```
+//!
+//! ```rust
+//! use rcurl::modules::rsyncd_config::RsyncdConfig;
+//! let config = RsyncdConfig::parse_str("port = 873\n[pub]\npath = /tmp", None).unwrap();
+//! assert_eq!(config.port, 873);
+//! assert!(config.modules.contains_key("pub"));
+//! ```
+
 pub mod cli;
 pub mod config;
 pub mod downloader;
